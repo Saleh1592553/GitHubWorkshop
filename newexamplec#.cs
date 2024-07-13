e4cb4c001072f6675519525fe224663a4cfce7a3
saleh fuad alrobaki <salhalrbaky@gmail.com>
2024-07-13 16:36:50 +0300

    create new c# file

---
+++ newexamplec#.cs
@@ -0,0 +1,52 @@
+
+using System;
+
+namespace newexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("examples in C#");
+            Console.WriteLine(Add(3, 5));
+            Console.WriteLine(Subtract(10, 4));
+            Console.WriteLine(Multiply(7, 6));
+            Console.WriteLine(Divide(20, 4));
+            Console.WriteLine(Square(5));
+        }
+
+        //add two numbers
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //subtract the second number from the first
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        //multiply two numbers
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        //divide the first number by the second
+        static int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+
+        //return the square of a number
+        static int Square(int a)
+        {
+            return a * a;
+        }
+    }
+}
